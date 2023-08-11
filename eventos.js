6df93d2880b9609957d6c0a5dfb9a20cb60ff106
EmilianoMozzone <emilianomozzone@hotmail.com>
2023-08-10 23:46:23 -0300

    he creado el eventos.js con el evento del boton

---
+++ eventos.js
@@ -0,0 +1,7 @@
+let divboton = document.getElementById('divboton');
+
+document.addEventListener ('DOMContentLoaded', function(){
+    divboton.addEventListener('click', function(){
+        alert('Hola! Soy el div');
+    });
+});
